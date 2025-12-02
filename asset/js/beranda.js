aa324e9a5c56bf464f63fad61b6e2bf3bfe504ed
mizukimura <randomnpc260@gmail.com>
2025-12-02 10:25:26 +0700

    Implement search on Enter key in search input
    Added functionality to search on Enter key press.

--- asset/js/beranda.js
+++ asset/js/beranda.js
@@ -3,15 +3,18 @@ $(document).ready(function () {
   // Tag filter
   $(".tag").click(function () {
     let filter = $(this).text().toLowerCase();
+    if ($(this).hasClass("reset-tag")) return; // Skip reset
+
     $(".card").hide().filter(function () {
       let tags = $(this).data("tags");
       return tags.includes(filter);
     }).fadeIn(200);
   });
+
   $(".reset-tag").click(function(){
-    $(".card").fadeIn(200)
-    $("#searchInput").val("")
-  })
+    $(".card").fadeIn(200);
+    $("#searchInput").val("");
+  });
 
   // Search
   $("#searchBtn").click(function () {
@@ -19,6 +22,13 @@ $(document).ready(function () {
     $(".card").hide().filter(function () {
       return $(this).find("h3").text().toLowerCase().includes(keyword);
     }).fadeIn(200);
+  });
+
+  // Search on Enter key
+  $("#searchInput").on("keyup", function(e){
+    if(e.key === "Enter"){
+      $("#searchBtn").click();
+    }
   });
 
 });
